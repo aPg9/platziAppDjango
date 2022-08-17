6d360b8288e2ad8bc3804d4366f0bcf3a8b7a33d
Adrian Posada <adrian-924@hotmail.com>
2022-08-16 19:04:40 -0500

    clase de creacion del template del home

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    lates_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {"latest_question_list": lates_question_list})
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estas viendo la pregunta numero {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta numero {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta numero {question_id}")
