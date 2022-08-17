6edeff645821912900e41df945d61f3e42d040be
Adrian Posada <adrian-924@hotmail.com>
2022-08-17 12:27:59 -0500

    implementacion de las generic views en la aplicacion

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -2,25 +2,44 @@ from django.urls import reverse
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from .models import Choice, Question
+from django.views import generic
 
 
-def index(request):
-    lates_question_list = Question.objects.all()
-    return render(request, "polls/index.html", {"latest_question_list": lates_question_list})
+# def index(request):
+#     lates_question_list = Question.objects.all()
+#     return render(request, "polls/index.html", {"latest_question_list": lates_question_list})
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/detail.html", {"question": question})
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {
-        "question": question
-    })
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, "polls/results.html", {
+#         "question": question
+    # })
 
 
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """Return the last five published questions"""
+        return Question.objects.order_by("-pub_date") [:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+class ResultlView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+
+# Function based view
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
